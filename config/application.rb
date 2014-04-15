53db834113d6f90fdb1cfc333352180aa4b38616
tbehailu <tsionbehailu@gmail.com>
2014-04-14 18:24:24 -0700

    changed links on main page

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # add fonts folder to assets path
 # config.assets.paths << Rails.root.join("assets", "fonts")
-config.assets.paths << "#{Rails.root}/app/assets/fonts"
+# config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
